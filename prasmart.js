998cccf52d4474f6e687f36a6f1d8d7714587a3f
Mhduzan <muhamamaduzan1234@gmail.com>
2024-11-04 01:32:35 +0700

    Add files via upload

---
+++ prasmart.js
@@ -0,0 +1,13 @@
+function tambah() {
+  var hasilb =0;
+  var hasila = 0;
+  for( var i =0; i < arguments.length; i++) {
+    hasila + hasilb += arguments[i];
+  }
+  return hasil;
+}
+
+var coba = tambah (parseInt(prompt('masukan nilai 1:')));
+
+
+console.log(hasil);
